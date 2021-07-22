cd91cdf7fc3288d25100fc26336adb9ee9355af0
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-07-22 14:28:44 +0200

    [Updated] project structure

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = env.bool("DEBUG", default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
