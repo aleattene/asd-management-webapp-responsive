c759bf9ab4008e0a9e696cc99d8ae087209f61e7
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-07-23 11:24:52 +0200

    [Added] new class "Athletes" and its migration

--- backend/athletes/models.py
+++ backend/athletes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 
+class Athletes(models.Model):
+    surname = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    birthdate = models.DateField()
+    state = models.BooleanField()
+
+    def __str__(self):
+        return self.surname + self.name + " born on " + self.birthdate + " (Active)"
+
