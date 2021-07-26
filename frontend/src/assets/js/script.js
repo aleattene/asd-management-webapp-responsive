dc598797289a812496a2d4352dbef02f46976849
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-07-26 11:19:38 +0200

    [Added] cookie setting in the function getAuthorizationCode

--- frontend/src/assets/js/script.js
+++ frontend/src/assets/js/script.js
@@ -1,16 +1,20 @@
 
 console.log('Script OK'); // test
+    ";
 
 function getAuthorizationCode() {
     var queryString = window.location.search;
     var urlParams = new URLSearchParams(queryString);
     var code = urlParams.get('code');
     var state = urlParams.get('state');
-    var element = document.getElementById("code_state");
+    var element = document.getElementById("auth_code");
+    document.cookie = "authorization_code = " + code;
     return element.innerHTML = "<br><br>" +
-    "Authorization code = " + code + "<br><br>" +
+    "Authorization code = " + code + "<br><br>"; //+
     "State = " + state;
 }
+
+
 
 console.log('END Script'); // test
 
