4326f5ca254e24bfef3f37cd5e583910821b8900
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-08-20 14:59:10 +0200

    [Updated] main class "APP" (React) and "Navbar"

--- frontend/asdmanagement/src/App.js
+++ frontend/asdmanagement/src/App.js
@@ -1,17 +1,48 @@
+import React, { Component } from "react";
 import "./App.css";
-import NavBar from "./components/navbar.js";
-import Carousel from "./components/carousel.js";
-//import ASD from "./logo.svg";
+import Navbar from "./components/navbar";
+import UserNavbar from "./components/user/user-navbar";
+import Carousel from "./components/carousel";
+import Login from "./components/login";
+import Card from "./components/card";
+import UserHandleAtletes from "./components/user/user-handle-athtletes";
+import UserServices from "./components/user/user-services";
+import UserProfile from "./components/user/user-profile";
+import OperatorNavbar from "./components/operator/operator-navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/home";
 
-function Login() {}
-
-function App() {
-  return (
-    <div class="container bg-light">
-      <NavBar />
-      <Carousel />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div class="container-fluid bg-lightbg-light">
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+          <Switch>
+            <Route exact path="/user" component={UserNavbar} />
+          </Switch>
+          <Switch>
+            <Route
+              exact
+              path="/user/handle-atletes"
+              component={UserHandleAtletes}
+            />
+          </Switch>
+          <Switch>
+            <Route exact path="/user/services" component={UserServices} />
+          </Switch>
+          <Switch>
+            <Route exact path="/user/handle-profile" component={UserProfile} />
+          </Switch>
+          <Switch>
+            <Route exact path="/operator" component={OperatorNavbar} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- frontend/asdmanagement/src/components/navbar.js
+++ frontend/asdmanagement/src/components/navbar.js
@@ -1,16 +1,16 @@
+import { Component } from "react";
 import ASD from "../assets/img/logo.jpg";
 
 // Navigation Bar
-function NavBar() {
-  return (
-    <div>
-      <nav className="navbar navbar-dark navbar-expand-md bg-dark">
-        <a className="navbar-brand" href="">
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark navbar-expand-md bg-dark">
           <img src={ASD} width="100rem" style={{ float: "left" }} />
-        </a>
-      </nav>
-    </div>
-  );
+        </nav>
+      </div>
+    );
+  }
 }
-
-export default NavBar;
+export default Navbar;
