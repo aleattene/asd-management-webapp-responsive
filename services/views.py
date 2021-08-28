28e6461f48e4738beb54e8279ec13b92de3b669a
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-08-28 16:05:04 +0200

    [Refactoring] from context_object_name "object_list" to "all_services_list"

--- services/views.py
+++ services/views.py
@@ -1,7 +1,10 @@
 from django.views.generic import ListView
 from .models import Service
 
+
 class ServiceListView(ListView):
     model = Service
-    template_name = 'services.html'
+    template_name = 'services/services.html'
+    context_object_name = 'all_services_list'
+
 
