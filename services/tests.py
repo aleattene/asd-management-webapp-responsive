7de5d7449977b26e1592107b6a37aa4f4c2442d5
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-08-28 17:28:10 +0200

    [Added] tests for the model "Service"

--- services/tests.py
+++ services/tests.py
@@ -1,9 +1,34 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
+from .models import Service
 
 
-class SimpleTests(SimpleTestCase):
+class ServiceModelTest(TestCase):
+
+    def setUp(self) -> None:
+        Service.objects.create(
+            description='Servizio di Prova',
+            price=10.00,
+            is_active=True
+        )
+
+    def test_description_content(self):
+        service = Service.objects.get(id=1)
+        expected_object_name = f'{service.description}'
+        self.assertEqual(expected_object_name, 'Servizio di Prova')
+
+    def test_price_content(self):
+        service = Service.objects.get(id=1)
+        expected_object_name = {service.price}
+        self.assertEqual(expected_object_name, 10.00)
+
+    def test_is_active_content(self):
+        service = Service.objects.get(id=1)
+        expected_object_name = {service.is_active}
+        self.assertEqual(expected_object_name, True)
 
     def test_services_page_status_code(self):
         response = self.client.get('services/')
         self.assertEqual(response.status_code, 200)
+
+
 
