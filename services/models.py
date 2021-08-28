61aadf8dc0466c68a0538a362bb953835ed2de17
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-08-28 15:59:33 +0200

    [Updated] Service attribute from "active_state" to "is_active"

--- services/models.py
+++ services/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Service(models.Model):
     description = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
-    active_state = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.description
