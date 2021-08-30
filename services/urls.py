98d5658d529f030460dbb7de23ea1b21bbf3723c
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-08-30 11:32:26 +0200

    [Added] new URL paths for entering and updating the services

--- services/urls.py
+++ services/urls.py
@@ -1,22 +1,8 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.urls import path, include
-from .views import ServiceListView
+from django.urls import path
+from .views import ServiceListView, ServiceAddView, ServiceUpdateView
 
 urlpatterns = [
     path('services/', ServiceListView.as_view(), name='services'),
+    path('services/add/', ServiceAddView.as_view(), name='services-add'),
+    path('services/<int:pk>/', ServiceUpdateView.as_view(), name='services-update'),
 ]
