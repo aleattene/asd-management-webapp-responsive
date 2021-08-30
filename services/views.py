6e1e9823d8c3b45b6081549895e58074c4cd8f07
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-08-30 11:31:31 +0200

    [Created] new classes/views for entering and updating the services

--- services/views.py
+++ services/views.py
@@ -4,7 +4,15 @@ from .models import Service
 
 class ServiceListView(ListView):
     model = Service
-    template_name = 'services/services.html'
+    template_name = 'services/services-list.html'
     context_object_name = 'all_services_list'
 
 
+class ServiceAddView(ListView):
+    model = Service
+    template_name = 'services/services-add.html'
+
+
+class ServiceUpdateView(ListView):
+    model = Service
+    template_name = 'services/services-update.html'
