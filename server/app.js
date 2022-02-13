dd6dfd6bb606a2c286358b0f84b8a2ed0f396206
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2022-02-13 23:28:55 +0100

    fix: modify env_variable PORT for Heroku deploy (error R10)

--- server/app.js
+++ server/app.js
@@ -18,7 +18,8 @@ app.set('view engine', 'ejs');
 
 app.use('/', homepage);
 
+const PORT = process.env.HOST_PORT
 
-app.listen(process.env.HOST_PORT, () => {
-    console.log(`Server running on port ${process.env.HOST_PORT} ... `)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} ... `)
 });
